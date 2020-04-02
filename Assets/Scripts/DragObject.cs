ed98f6d0afda49a057836c37dc2e1bc0d8d58d02
MarekD97 <mdorosz2@wp.pl>
2020-04-02 13:01:23 +0200

    Add drag object

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    public Transform moveThis;
+    //the layers the ray can hit
+    public LayerMask hitLayers;
+
+    void OnMouseDown()
+    {
+        moveThis = gameObject.transform;
+        hitLayers = LayerMask.GetMask("Plane");
+    }
+    void OnMouseDrag()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers))
+        {
+            moveThis.transform.position = hit.point;
+        }
+    }
+}
