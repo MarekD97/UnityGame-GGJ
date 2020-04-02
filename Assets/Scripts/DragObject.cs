af2998753a8374a2d0568afce9cfc9168f46a439
Marek <50573782+MarekD97@users.noreply.github.com>
2020-04-02 17:48:44 +0200

    Add drag object, random generator, animations (#2)
    * Add ground and character
    
    * Add animation for character
    
    * Change character animation
    
    * Add drag object
    
    * Add random character generator and animations

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    //the layers the ray can hit
+    public LayerMask hitLayers;
+
+    void OnMouseDown()
+    {
+    }
+    void OnMouseUp()
+    {
+
+    }
+    void OnMouseDrag()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers))
+        {
+            gameObject.transform.position = hit.point;
+        }
+    }
+}
